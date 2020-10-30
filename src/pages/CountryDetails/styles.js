13ebd774d17bccc86c884c110834d4cbb60a64e8
Hermes Caretta <38593591+HermesCaretta@users.noreply.github.com>
2020-10-30 12:06:49 -0300

    a few more changes

--- src/pages/CountryDetails/styles.js
+++ src/pages/CountryDetails/styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
   background: ${(props) =>
     props.theme.mode === 'dark' ? 'hsl(207, 26%, 17%)' : 'hsl(0, 0%, 98%)'};
-  width: 100%;
+  min-width: 100vw;
   min-height: 100vh;
   display: flex;
   flex-direction: column;
